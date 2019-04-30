82cf925f384ff3e3ae94d44eea9817945de4bc17
Kenneth-ca <kenneth.ca95@gmail.com>
2019-04-30 20:40:43 +0000

    Add conditions for last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+last = abs(number) % 10
 if number < 6 and number != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, last))
+    if last == 0:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+              .format(number, last))
+    else:
+        print("Last digit of {:d} is -{:d} and is less than 6 and not 0"
+              .format(number, last))
 elif number > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
           .format(number, last))
