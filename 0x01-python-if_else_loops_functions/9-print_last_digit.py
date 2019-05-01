89337996b6e851f9870d2ee6f8be89657df85210
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-01 00:50:02 +0000

    Prints the last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        print(number % 10, end='')
+        return (number % 10)
+    else:
+        print(abs(number) % 10, end='')
+        return (abs(number) % 10)
