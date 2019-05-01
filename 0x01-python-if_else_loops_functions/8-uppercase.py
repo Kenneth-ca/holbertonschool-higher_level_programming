151761d4052afff51a206eb4c1b7bb9c70098a2e
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-01 02:11:42 +0000

    Prints to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for s in str:
+        if (ord(s) > 96) and (ord(s) < 123):
+            aux = ord(s) - 32
+        else:
+            aux = ord(s)
+        print(chr(aux), end="")
+    print()
