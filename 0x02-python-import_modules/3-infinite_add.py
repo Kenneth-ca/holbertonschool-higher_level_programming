b38555be33dfb31b9b71ee1d3972af2bcc6f1b02
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-03 03:03:30 +0000

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0")
+    else:
+        total = 0
+        for a in range(1, len(argv)):
+            total += int(argv[a])
+        print("{:d}".format(total))
