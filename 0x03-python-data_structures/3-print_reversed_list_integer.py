1e3c55950cffc9e0888b5c79ef0125a36d0ab809
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-07 20:32:58 +0000

    [Alt] using property of list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev = reversed(my_list)
-    for m in rev:
+    my_list.reverse()
+    for m in my_list:
         print("{:d}".format(m))
