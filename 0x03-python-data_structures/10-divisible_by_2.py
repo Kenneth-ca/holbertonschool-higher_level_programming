e003257c68b17c1552b9e4ae66175eb89328a0d2
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-08 01:38:46 +0000

    [Fix] removed len

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = []
-    for m in range(len(my_list)):
+    for m in my_list:
         if m % 2 == 0:
             new.append(True)
         else:
