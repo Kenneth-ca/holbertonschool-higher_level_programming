7c284878f50420ad7601d6cd037757fc15c7c718
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-09 22:57:04 +0000

    [Fix] all ocurrencies

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,8 @@ def search_replace(my_list, search, replace):
     if my_list is not None:
         new = []
         new = my_list.copy()
-        idx = new.index(search)
-        new.remove(search)
-        new.insert(idx, replace)
+        for i in range(len(new)):
+            if new[i] == search:
+                new.pop(i)
+                new.insert(i, replace)
         return new
