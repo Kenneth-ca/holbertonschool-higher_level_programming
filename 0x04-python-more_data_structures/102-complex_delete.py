daf3aa4c0d9a31dd45e31d5508ff7a86624210e5
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-10 04:00:00 +0000

    [Fix] return in all conditions

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if (a_dictionary is not None) and (len(a_dictionary) is not 0):
+    if value in a_dictionary.values():
         where = []
         for keys, val in a_dictionary.items():
             if val == value:
                 where.append(keys)
         for w in where:
             a_dictionary.pop(w)
-        return a_dictionary
+    return a_dictionary
