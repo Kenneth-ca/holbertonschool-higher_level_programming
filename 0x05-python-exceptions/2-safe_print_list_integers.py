fd00251bbde5b8739622fc535541226dc2241eb4
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-20 19:53:44 +0000

    A function that prints integers from a list of many types of elements

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    if my_list is None:
+        return 0
+    cont = 0
+    for m in range(x):
+        try:
+            print("{:d}".format(my_list[m]), end="")
+            cont += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return cont
