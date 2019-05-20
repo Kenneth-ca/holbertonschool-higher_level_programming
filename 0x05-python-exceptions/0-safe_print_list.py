ac3dd3853535888a3f0e07dfde74a4a498ad1d8a
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-20 17:59:49 +0000

    Safe prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list is None:
+        return 0
+    cont = 0
+    try:
+        for ele in range(x):
+            print("{}".format(my_list[ele]), end="")
+            cont += 1
+        print()
+    except IndexError:
+        print()
+        pass
+    return cont
