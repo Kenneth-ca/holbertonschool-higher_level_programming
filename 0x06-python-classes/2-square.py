5cc5a76482343f912a2eccfc3e66c61e23234d0e
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-21 23:14:49 +0000

    Square with validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        try:
+            if isinstance(size, int) is False:
+                raise TypeError
+            if size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer", end="")
+            raise Exception
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise Exception
+        self.__size = size
