3c45df5c75b10885a6f4a6c8f4a144657610d956
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-22 01:59:17 +0000

    [Fix] inequality (<)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,7 +41,7 @@ class Square:
 
     @size.setter
     def position(self, value):
-        if value[0] >= 0 and value[1] >= 0:
+        if value[0] < 0 and value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         if isinstance(value[0], int) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
