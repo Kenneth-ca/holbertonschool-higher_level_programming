ac0ce88bd84c96f7a8694003eb02d798fcda0162
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-22 01:54:21 +0000

    Coodinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            if isinstance(value, int) is False:
+                raise TypeError
+            if value < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer", end="")
+            raise Exception
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise Exception
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for j in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        if value[0] >= 0 and value[1] >= 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(value[0], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(value[1], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
