631b656b654ce06c7210f94e74a09362a53850e0
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-23 02:14:38 +0000

    Compare 2 squares

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            if isinstance(value, int) is False:
+                raise TypeError
+            if value < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer", end="")
+            raise Exception
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise Exception
+        self.__size = value
+
+    def area(self):
+        return self.__size * other.__size
+    def __eq__(self, other):
+        return self.__size * 2 == other.__size *2
+    def __ne__(self, other):
+        return self.__size * 2 != other.__size *2
+    def __lt__(self, other):
+        return self.__size * 2 < other.__size *2
+    def __le__(self, other):
+        return self.__size * 2 <= other.__size *2
+    def __gt__(self, other):
+        return self.__size * 2 > other.__size *2
+    def __ge__(self, other):
+        return self.__size * 2 >= other.__size *2
