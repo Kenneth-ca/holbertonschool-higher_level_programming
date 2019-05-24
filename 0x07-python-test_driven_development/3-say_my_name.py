08dcdf668ee0b657ff1b4945477827a48e8bc089
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-24 16:06:34 +0000

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,13 +2,12 @@
 """
     Module to say a name
 """
-import pdb
+
 
 def say_my_name(first_name, last_name=""):
     """ A function that says a name"""
-    pdb.set_trace()
     if first_name is None or type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if last_name is None or type(first_name) is not str:
+    if last_name is None or type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
