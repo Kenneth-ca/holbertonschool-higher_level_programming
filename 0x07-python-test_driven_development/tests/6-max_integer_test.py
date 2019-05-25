fb8f1f6550e0c02481de048fe388005af07c58bf
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-25 00:35:23 +0000

    [Add] unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,14 +4,31 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
 
     def test_work(self):
+        """Normal output"""
         a_list = [1, 2, 3]
         self.assertEqual(3, max_integer(a_list))
 
     def test_empty(self):
+        """empty case"""
         self.assertEqual(None, max_integer([]))
+
+    def test_none(self):
+        """none"""
+        self.assertRaises(TypeError, max_integer([]))
+
+    def test_none2(self):
+        """none v2"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_negative(self):
+        """negative"""
+        a_list_n = [-1, -2, -3]
+        self.assertEqual(-1, max_integer(a_list_n))
 
 if __name__ == '__main__':
         unittest.main()
