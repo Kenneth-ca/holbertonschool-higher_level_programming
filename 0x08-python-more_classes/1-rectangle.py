3a62c586098519af443d6fab7b1a1de7d83ae70f
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-28 13:57:55 +0000

    [Fix] Put conditions in __init__ too

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,14 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+        if isinstance(width, int) is not True:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if isinstance(height, int) is not True:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
