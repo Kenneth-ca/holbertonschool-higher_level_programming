094ade74e05b29e9af5acdb3f1a03982518a8be6
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-28 00:51:45 +0000

    Fix condition for case 0

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
         return 2 * self.__height + 2 * self.__width
 
     def __str__(self):
-        if self.__height < 0 or self.__width < 0:
+        if self.__height <= 0 or self.__width <= 0:
             return ""
         str_h = ""
         for i in range(self.__height):
