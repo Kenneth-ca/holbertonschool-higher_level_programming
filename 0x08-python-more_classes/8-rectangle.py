5152d149bbfcb35cfbc2df804681e88e01fb4f38
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-28 03:16:46 +0000

    Add decorator @staticmethod

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -62,6 +62,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if isinstance(rect_1, Rectangle) is not True:
             TypeError("rect_1 must be an instance of Rectangle")
