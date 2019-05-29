7e2d3a8b0e48fd3e0c9e5060d6db5d0ab086a3ac
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-29 01:52:26 +0000

    Add the last task (clear strings)

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def magic_string():
+    super_string = ""
+    for i in range(n):
+        if i == 0 or i == n:
+            super_string += "Holberton"
+        else:
+            super_string += "Holberton, " * i
+    return super_string
