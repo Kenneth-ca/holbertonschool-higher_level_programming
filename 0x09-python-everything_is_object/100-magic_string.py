bb1c353314eb70dcbd029d3942800e9a6c1804ad
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-29 02:32:24 +0000

    Magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    super_string = ""
-    for i in range(n):
-        if i == 0 or i == n:
-            super_string += "Holberton"
-        else:
-            super_string += "Holberton, " * i
-    return super_string
+def magic_string(number=[0]):
+    number[0] = number[0] + 1
+    return "Holberton, " * (number[0] - 1) + "Holberton, "[:-2]
