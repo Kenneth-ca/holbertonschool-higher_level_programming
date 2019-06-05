8a36ea5e148ca4d8231d68007dd56214ac1db2cf
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-05 20:07:33 +0000

    Read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    counter = 0
+    with open(filename, "r") as f:
+        for line in f:
+            if counter >= nb_lines and nb_lines > 0:
+                break
+            counter += 1
+            print(line, end="")
