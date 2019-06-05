f8ee971eb5e979c0eb4b116d1ccf5f66d7cf815d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-05 03:08:59 +0000

    [Fix] right super() use

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -36,11 +36,4 @@ class Square(Rectangle):
     """Square that inherits Rectangle"""
 
     def __init__(self, size):
-        super().integer_validator("size", size)
-        self.__size = size
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
-
-    def area(self):
-        return self.__size * self.__size
+        super().__init__(size, size)
