c7151bf12aea4607b720d479b3678fe0ef0babc4
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-06 01:45:55 +0000

    Reformulating exercise, opening file, using both tasks

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,6 +5,14 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 new_list = []
+
+try:
+    var_file = load_from_json_file("add_item.json")
+    for i in var_file:
+            new_list.append(i)
+except:
+    pass
+
 for i in range(len(argv)):
     if i != 0:
         new_list.append(argv[i])
