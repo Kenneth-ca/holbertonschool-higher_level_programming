713b3b10572872ff47bb58f96dbd327eaee97076
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-06 01:36:10 +0000

    Load, add, save

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+from sys import argv
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+new_list = []
+for i in range(len(argv)):
+    if i != 0:
+        new_list.append(argv[i])
+save_to_json_file(new_list, "add_item.json")
