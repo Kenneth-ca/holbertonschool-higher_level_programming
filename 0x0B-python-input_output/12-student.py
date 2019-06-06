c39e7d10d0930cea954d05d6dd7870a0e9c6b2e9
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-06 03:09:36 +0000

    Student to JSON with filter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) is not True:
+            return self.__dict__
+        else:
+            store_dict = self.__dict__
+            only_name = {}
+            for keys, value in store_dict.items():
+                for i in attrs:
+                    if keys == i:
+                        only_name[keys] = value
+            return only_name
