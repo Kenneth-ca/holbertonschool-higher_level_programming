b56b771eaad31f67ad42463f96bbc60c271647f5
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-10 21:18:33 +0000

    T5: Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -18,7 +18,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -30,7 +30,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -42,7 +42,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        return self.y
+        return self.__y
 
     @y.setter
     def y(self, value):
@@ -75,3 +75,7 @@ class Rectangle(Base):
 
     def area(self):
         return self.__width * self. __height
+
+    def display(self):
+        for i in range(self.height):
+            print("#" * self.__width)
