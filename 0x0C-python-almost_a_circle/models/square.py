c6b09f004c4719da726957d00a10cc670f7f1f7d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-11 21:41:41 +0000

    T11: Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,8 +7,20 @@ class Square(Rectangle):
     """A square that inherits a rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Init for the Square"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Property for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+
     def __str__(self):
+        """Str for square"""
         return ("[Square] ({}) {}/{} - {}".
                 format(self.id, self.x, self.y, self.width))
