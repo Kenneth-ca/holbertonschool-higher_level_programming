22a53faed5ac3c8ad42c7b3fde971147005b2b3f
Kenneth-ca <kenneth.ca95@gmail.com>
2019-06-12 00:37:52 +0000

    Testing with staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Dictionary to JSON string"""
         if list_dictionaries is None or list_dictionaries == []:
