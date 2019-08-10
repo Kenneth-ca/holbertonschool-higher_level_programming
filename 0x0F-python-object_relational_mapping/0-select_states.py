9c6d4f44b6f4dbc9c1e2dac2fb6cd5573b8bb0d4
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-10 02:01:42 +0000

    Many files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa"""
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2],
+                         db=argv[3])
     var = db.cursor()
     var.execute("SELECT * FROM states")
     res = var.fetchall()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""a script that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2],
+                         db=argv[3])
+    var = db.cursor()
+    var.execute("SELECT * FROM states\nWHERE name like 'N%'")
+    res = var.fetchall()
+    for i in res:
+        print(i)
