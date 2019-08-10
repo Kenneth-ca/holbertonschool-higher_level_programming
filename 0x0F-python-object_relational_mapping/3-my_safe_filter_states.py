c48b03cce0b3959ce41a169d35fdea3e2679524a
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-10 01:29:55 +0000

    Protection against injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Protection against injection"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2],
+                         db=argv[3])
+    var = db.cursor()
+    var.execute("SELECT * FROM states\nWHERE %(id)s like name", {'id': argv[4]})
+    res = var.fetchall()
+    for i in res:
+        print(i)
