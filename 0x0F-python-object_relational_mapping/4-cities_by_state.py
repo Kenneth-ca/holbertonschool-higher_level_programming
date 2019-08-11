60b5df047e689ccf248fae3af22c5edeb65b2553
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-11 19:16:15 +0000

    Test

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2],
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306)
     var = db.cursor()
     var.execute("SELECT cities.id, cities.name, states.name FROM cities\
