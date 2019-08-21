6d7f7f6e21d3d3636e989265553b18818870746d
Kenneth-cad <kenneth.ca95@gmail.com>
2019-08-21 00:40:52 +0000

    Not just testing but taking the argument

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods
-curl -si -X OPTIONS --head 0.0.0.0:5000/route_4 | grep Allow | cut -d" " -f 2-5
+curl -si -X OPTIONS $1 | grep Allow | cut -d" " -f 2-5
