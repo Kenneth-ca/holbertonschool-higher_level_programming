aae9ad897b98fa35753818cc9ca60a17aec16b45
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-27 02:57:14 +0000

    A bit desperate...

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,12 +3,12 @@
 It takes a URL, sends a request and displays the value of a variable
 """
 import urllib.request
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
     try:
-        url = argv[1]
+        url = sys.argv[1]
         with urllib.request.urlopen(url) as response:
             print(response.info()['X-Request-Id'])
     except:
