d8199c8da29c01d79fff3db2251470e48134d845
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-27 04:22:14 +0000

    Comments

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-It takes a URL, sends a request and displays the value of a variable
+Takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response
+(decoded in utf-8)
 """
 import urllib.request
 import sys
