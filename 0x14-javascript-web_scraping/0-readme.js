87d780538e9119f353fd52cd21d95a2e7971ab3b
Kenneth-ca <kenneth.ca95@gmail.com>
2019-09-24 20:04:11 +0000

    fix statement

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-  }
-  if (data) {
+  } else {
     console.log(data);
   }
 });
