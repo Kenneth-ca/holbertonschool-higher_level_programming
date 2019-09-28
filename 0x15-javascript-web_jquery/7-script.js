d7ef8f3d49daaaa25a0546bb672b499268a8932d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-09-28 14:57:57 -0500

    [add] using ajax

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+$.ajax({
+    url: "https://swapi.co/api/people/5/?format=json",
+    type: "GET",
+    dataType : "json",
+})
+    .done(function( json ) {
+        $( "DIV#character" ).text( json.name );
+    });
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+$.ajax({
+    url: "https://swapi.co/api/films/?format=json",
+    type: "GET",
+    dataType : "json",
+})
+    .done(function( json ) {
+        for (val of json.results) {
+            $( "UL#list_movies" ).append("<li>" + val.title + "</li>");
+        }
+    });
